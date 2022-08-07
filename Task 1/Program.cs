8f4525b4ffc63995d492bf26b543f5901b5cef7d
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-07 14:28:25 +0300

    Задание 1 тру

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое целое число: " );
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: " );
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number2 * number2;
+
+if(square == number1) // если равно
+{
+Console.Write($"Число: {number1} является квадратом: {number2}! ");
+}
+if(square != number1) // если не равно
+{
+Console.Write($"Число: {number1} НЕ является квадратом: {number2}! ");
+}
