a0e001a49793deb120c0898c8dba1d28c54631c1
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-07 14:46:14 +0300

    Дз задание готово

---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое целое число: " );
+var number1 = Decimal.Parse(Console.ReadLine());
+Console.Write("Введите второе целое число: " );
+var number2 = Decimal.Parse(Console.ReadLine());
+
+if(number1 > number2) 
+{
+Console.Write($"максимальное число {number1} ");
+}
+if(number1 < number2)
+{
+Console.Write($"Mаксимальное число {number2} ");
+}
+if(number1 == number2)
+{
+Console.Write("Числа равны! ");
+}
