748d04b9550bc8776e219a6cd33541aa416bd00d
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-08 15:56:59 +0300

    задания 4 и 6

---
+++ Task 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace task4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите числа через enter: ");
+            int[] array = new int[3];
+ 
+            for (int i = 0; i < 3; i++)
+                array[i] = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.WriteLine("Максимальное число: " + Max(array).ToString());
+            Console.ReadLine();
+        }
+        static int Max(int[] array)
+        {
+            int max;
+            return max = array.Max();
+        }
+    }
+}
---
+++ Task 6/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите число: " );
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num % 2 == 0 ? "Число четное!" : "Число нечетное!");
