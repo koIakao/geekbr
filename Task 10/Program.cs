bb939e566ab6c54cf6d4d9848eace68dd185a454
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-10 12:41:20 +0300

    10-11

---
+++ Task 10/Program.cs
@@ -0,0 +1,12 @@
+﻿int showMiddleDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = firstDigit % 10;
+    int result = secondDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+int res = showMiddleDigit(number);
+Console.WriteLine($"Сгенерированное число {number}. Обработанное число -> {res}");
---
+++ Task 11/Program.cs
@@ -0,0 +1,12 @@
+﻿int deleteMiddleDigit (int num)
+{
+    int firstDigit = num / 100;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+int number = new Random().Next(100, 1000);
+
+int res = deleteMiddleDigit(number);
+Console.WriteLine($"Сгенерированное число {number}. Обработанное число -> {res}");
