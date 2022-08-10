b7deeee928e9cdd5d32a25bcd2ab1c80cbb06e26
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-10 11:16:48 +0300

    Task 9

---
+++ Task 9/Program.cs
@@ -0,0 +1,24 @@
+﻿int numb = new Random().Next(10, 100);
+Console.WriteLine($"New random number from [10 - 99] => {numb}");
+
+int firstSymb = numb / 10;
+int secondSymb = numb % 10;
+
+if (firstSymb == secondSymb) Console.WriteLine($"Numbers {firstSymb} & {secondSymb} are equial!");
+else
+{
+    int max = firstSymb > secondSymb ? firstSymb : secondSymb;
+    Console.WriteLine($"Greatest number of {numb} is {max}");
+}
+
+//
+// int MaxDigit(int num)
+//{
+   //  int firstSymb = num / 10;
+   // int secondSymb = num % 10;
+   // return firstSymb > secondSymb ? firstSymb : secondSymb;
+//}
+
+//int MaxDigit = MaxDigit (numb);
+//Console.WriteLine($"Greatest number of {numb} is {MaxDigit}");
+// }
