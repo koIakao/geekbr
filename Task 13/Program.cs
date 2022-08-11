f40b67b993d880782086808821bf17772e464637
Daniil Varshaver <d.varshaver@gmail.com>
2022-08-11 12:51:17 +0300

    13 15

---
+++ Task 13/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Решить без использования строк.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Write your number (>100): ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+int firsborder = 99;
+
+    if (userNum <= firsborder)
+    {
+        Console.WriteLine("incorrect number!");
+    }
+
+    else
+    {
+        int digitCount = (int)Math.Log10(userNum) + 1;
+        do
+        {
+            if (digitCount > 3) userNum = userNum / 10;
+        }
+        while (digitCount == 3);
+
+    int result = userNum % 10;
+    Console.WriteLine($"Result: {result}");
+    }
---
+++ Task 15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Укажите день недели [1-7]: ");
+int usernum = Convert.ToInt32(Console.ReadLine());
+if (usernum >= 8 || usernum <= 0)
+{
+    Console.WriteLine("Вы изобрели новый день недели?");
+}
+else
+{
+    bool Del(int num)
+    {
+        return num == 6 || num == 7;
+    }
+    bool result = Del(usernum);
+    Console.WriteLine(result ? "День выходной!" : "Опять на работу :(");
+}
